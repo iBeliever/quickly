09067251a6de5d0b38a950da59ce21dbbf9cc51c
Michael Spencer <sonrisesoftware@gmail.com>
2016-03-12 14:54:07 -0600

    Add a polyfill for String.prototype.includes
    Taken from the MDN documentation

--- src/string.js
+++ src/string.js
@@ -16,3 +16,17 @@ if (!String.prototype.startsWith) {
     return this.substr(position, searchString.length) === searchString;
   };
 }
+
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
