2755d10a09f62caf84a88e173c436cd4b0c29e22
Michael Spencer <sonrisesoftware@gmail.com>
2016-03-13 18:57:14 -0500

    Better organization of the polyfills

---
+++ src/polyfills/polyfills.js
@@ -0,0 +1,14 @@
+import './core/array'
+import './core/number'
+import './core/object'
+import './core/string'
+
+export {WeakMap, Map, WeakSet, Set} from './core/collections'
+export {Reflect} from './core/reflect'
+export {Symbol} from './core/symbol'
+
+export {fetch, Request, Response, Headers} from './fetch'
+
+export {Promise} from './promise/promise'
+
+export {setTimeout} from './timeout'
