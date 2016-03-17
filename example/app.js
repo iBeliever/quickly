06a3b026d5a63c505aac1dd306e856364b1b3ff6
Michael Spencer <sonrisesoftware@gmail.com>
2016-03-16 20:45:35 -0500

    Add a fully working example

--- example/app.js
+++ example/app.js
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import url from 'url'
 
 export class Person {
     constructor(name) {
@@ -13,14 +12,4 @@ export class Person {
 
 export function chunky(array) {
     return _.chunk(array, 3)
-}
-
-export function urlHost(url) {
-    const data = url.parse(url)
-    return data.host
-}
-
-export function getUsers() {
-    return fetch('http://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
 }
--- example/main.qml
+++ example/main.qml
@@ -7,11 +7,5 @@ Item {
         person.hello() // Prints "Hello, Michael!"
 
         console.log(JSON.stringify(App.chunky(['A', 'B', 'C', 'D', 'E', 'F', 'G'])))
-        console.log(App.urlHost('http://www.google.com'))
-
-        App.getUsers().then(function(users) {
-            var name = users[0].name
-            console.log('The first user is: ' + name)
-        })
     }
 }
